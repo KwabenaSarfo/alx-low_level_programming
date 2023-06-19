0d6bfbef17585a5c164eb879a13d3d3657dc3cfd
KwabenaSarfo <ksarfo195@gmail.com>
2023-06-19 13:50:25 -0700

    6 print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This code is for printing numbers from 0 to 9.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
