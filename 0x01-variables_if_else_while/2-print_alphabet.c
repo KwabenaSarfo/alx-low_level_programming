061089c684e02731758d0d371e7fa1ffe6854b06
KwabenaSarfo <ksarfo195@gmail.com>
2023-06-19 11:42:28 -0700

    2 print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This code is for printing alphabet.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z') {
+			putchar(b);
+		}
+	putchar('\n');
+	return (0);
+}
