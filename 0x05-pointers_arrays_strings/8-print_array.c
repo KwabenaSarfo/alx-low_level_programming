7e2f60371372b10ff5d61f4fa8d183159674c50b
KwabenaSarfo <ksarfo195@gmail.com>
2023-06-27 15:26:25 -0700

    8 print

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_array - The project creates a function 
+ * print_array - The project creates a function
  * that prints n elements of an array
  * @c: array name
- * @e: the number of elements,the array to be printed
+ * @e: the number of elements, the array to be printed
  * Return: c and e inputs
  */
 void print_array(int *c, int e)
