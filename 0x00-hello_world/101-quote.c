bcc10d19f121b963ee286121c2b6e266f7c833b0
KwabenaSarfo <ksarfo195@gmail.com>
2023-07-14 15:52:53 -0700

    New commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This program is for printing to standard error
+ * return 1 (on success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+New  C - Hello, World
