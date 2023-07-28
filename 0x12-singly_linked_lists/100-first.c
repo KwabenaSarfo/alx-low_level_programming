f6be545b31731d71ccbab6ce353ee7a0f5884fe3
KwabenaSarfo <ksarfo195@gmail.com>
2023-07-28 02:54:24 -0700

    100_first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,10 @@
 void first(void) __attribute__ ((constructor));
 
 /**
-* firstpri - This project prints a sentence before the main
+* first- This project prints a sentence before the main
 * function is executed
 */
-void firstpri(void)
+void first(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
