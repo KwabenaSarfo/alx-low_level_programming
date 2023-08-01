7810d0477daf6ca72c17e9712a4953666155793e
KwabenaSarfo <ksarfo195@gmail.com>
2023-08-01 13:05:02 -0700

    reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,18 +8,18 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-        listint_t *prev = NULL;
-        listint_t *next_node = NULL;
+	listint_t *prev = NULL;
+	listint_t *next_node = NULL;
 
-        while (*head)
-        {
-                next_node = (*head)->next;
-                (*head)->next = prev;
-                prev = *head;
-                *head = next_node;
-        }
+	while (*head)
+	{
+		next_node = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next_node;
+	}
 
-        *head = prev;
+	*head = prev;
 
-        return (*head);
+	return (*head);
 }
