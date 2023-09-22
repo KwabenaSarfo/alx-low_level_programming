bb776b4208f5b042843c6b66321e0edf51060ba3
KwabenaSarfo <ksarfo195@gmail.com>
2023-09-22 16:31:10 -0700

    newUpdate

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,7 +23,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->lengOne = lengOne;
+	new->len = lengOne;
 	new->next = NULL;
 
 	if (*head == NULL)
