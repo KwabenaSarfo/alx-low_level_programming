98fe40b629f5e82d7c7e909ecd75712b74962039
KwabenaSarfo <ksarfo195@gmail.com>
2023-09-26 14:04:51 -0700

    new

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,22 +1,33 @@
+#include <stdio.h>
 #include <stdlib.h>
-/**
- * add_nodeint - adds a new node
- * @head: pointer
- * @n: data to insert
- *
- * Return: pointer to the new node.
- */
+
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new12;
+    listint_t *new12_node;
 
-	new12 = malloc(sizeof(listint_t));
-	if (!new12)
-		return (NULL);
+    new12_node = malloc(sizeof(listint_t));
+    if (!new12_node)
+        return (NULL);
 
-	new12->n = n;
-	new12->next = *head;
-	*head = new12;
+    new12_node->n = n;
+    new12_node->next = *head;
+    *head = new12_node;
 
-	return (new12);
+    return (new12_node);
+}
+
+int main(void)
+{
+    listint_t *head = NULL;
+
+    add_nodeint(&head, 42);
+    add_nodeint(&head, 21);
+
+    return (0);
 }
